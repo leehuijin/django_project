0131638f988c0529ca0eecad6287df4770589b79
leehuijin <ksh05262@naver.com>
2020-07-05 10:31:46 +0900

    Initial commit

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -28,7 +30,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(instance=post)
+        form = PostForm(instance=post,)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
