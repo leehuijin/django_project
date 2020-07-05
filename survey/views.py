9c3309d794104c922f798e03697832b7477ae3ee
leehuijin <ksh05262@naver.com>
2020-07-05 15:57:09 +0900

    Added views to create/edit blog post inside the site.

--- survey/views.py
+++ survey/views.py
@@ -7,25 +7,25 @@ def index(request):
     latest_question_list=Question.objects.all().order_by('-pup_date')[:5]
     context={'latest_question_list':latest_question_list}
     print(latest_question_list)
-    return render(request, 'polls/index.html', context)
+    return render(request, 'survey/index.html', context)
 
 def detail(request, question_id):
     question= get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question': question})
+    return render(request, "survey/detail.html", {'question': question})
 
 def vote(request, question_id):
     question=get_object_or_404(Question, pk=question_id)
     try:
         selected_choice=question.choice_set.get(pk=request.POST['choice'])
     except(KeyError,Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',{'question':question,'error_message':"You didn't select a choice"})
+        return render(request, 'survey/detail.html',{'question':question,'error_message':"You didn't select a choice"})
 
     else:
         selected_choice.votes +=1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+        return HttpResponseRedirect(reverse('survey:results',args=(question_id,)))
 
 def results(request,question_id):
         question=get_object_or_404(Question, pk=question_id)
         test = [1,2,3,4,5]
-        return render(request, 'polls/results.html',{'question':question, 'test': test})
+        return render(request, 'survey/results.html',{'question':question, 'test': test})
